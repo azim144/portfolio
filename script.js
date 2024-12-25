9908bf26b6da02cfe96945061cef67285cd943fb
azim144 <azimjonrazzoqov92@gmail.com>
2024-12-25 14:53:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Portfolio sahifasiga dinamik funksiya qo'shish
+console.log("Portfolio sahifangiz muvaffaqiyatli yuklandi!");
+
+// "Haqimda" bo'limiga interaktivlik qo'shish
+document.querySelector("#about").addEventListener("click", () => {
+  alert("Men haqida ko'proq ma'lumot olish uchun mening loyihalarimni ko'rib chiqing!");
+});
